5bceb2c641a1081a0fee1737a0983887abe44300
BryanMartinez-Bautista <bryanmartinez1283@gmail.com>
2026-07-06 16:45:33 -0400

    Added BatmanCharacter code

--- src/main/java/com/CSC340/Assignment3/posts/BatmanCharacter.java
+++ src/main/java/com/CSC340/Assignment3/posts/BatmanCharacter.java
@@ -1,5 +1,65 @@
 package com.CSC340.Assignment3.posts;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "characters")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class BatmanCharacter {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long characterId ;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private String abilities;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    private String firstAppearance;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdAt;
+
+    public BatmanCharacter(String name, String role, String abilities, String imageUrl, String description, String firstAppearance) {
+        this.name = name;
+        this.role = role;
+        this.abilities  = abilities;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.firstAppearance = firstAppearance;
+    }
+
+    @PrePersist
+    @PreUpdate
+    protected void onUpdate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
