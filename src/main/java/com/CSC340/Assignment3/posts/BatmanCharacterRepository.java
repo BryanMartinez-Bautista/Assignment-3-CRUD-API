fb3ad649619f91293b7f7a0efaa363de9734908d
BryanMartinez-Bautista <bryanmartinez1283@gmail.com>
2026-07-06 21:13:58 -0400

    Added code to BatmanCharacterRepository file

--- src/main/java/com/CSC340/Assignment3/posts/BatmanCharacterRepository.java
+++ src/main/java/com/CSC340/Assignment3/posts/BatmanCharacterRepository.java
@@ -1,5 +1,15 @@
 package com.CSC340.Assignment3.posts;
 
-public class BatmanCharacterRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BatmanCharacterRepository extends JpaRepository<BatmanCharacter, Long> {
+
+    List<BatmanCharacter> findByRoleContainingIgnoreCase(String role);
+
+    List<BatmanCharacter> findByNameContainingIgnoreCase(String name);
+
 }
