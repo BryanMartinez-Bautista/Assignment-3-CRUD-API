3ced1f1c47092655759f3c512a9f7d3a9850e2ad
BryanMartinez-Bautista <164562829+BryanMartinez-Bautista@users.noreply.github.com>
2026-07-13 23:03:57 -0400

    Update BatmanCharacterUiController.java

--- src/main/java/com/CSC340/Assignment3/posts/BatmanCharacterUiController.java
+++ src/main/java/com/CSC340/Assignment3/posts/BatmanCharacterUiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-//Rest Controller
+//Controller
 @Controller
 @RequestMapping("/batmanCharacters")
 public class BatmanCharacterUiController {
